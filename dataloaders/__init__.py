17efa9c0d721b24b0410e4460c34f25b9b750b29
SanazNajiyan <45420543+SanazNajiyan@users.noreply.github.com>
2022-03-17 08:19:58 +0100

    reading path resolved

--- dataloaders/__init__.py
+++
@@ -1,4 +0,0 @@
-from dataloaders.utility import get_configs
-
-
-CONFIGS = get_configs("./dataloaders/config.ini")
--- dataloaders/dataloaders.py
+++ dataloaders/dataloaders.py
@@ -1,11 +1,14 @@
-#
+
+"""
+
+"""
 
 import pandas as pd
 from dataloaders.utility import get_configs
 from typing import Dict
+import os
 
-
-CONFIGS = get_configs("./config.ini")
+CONFIGS = get_configs(os.path.dirname(os.path.abspath(__file__)) + "\\config.ini")
 
 def dataloader(dataset: str) -> Dict[str, pd.DataFrame]:
     """
@@ -13,12 +16,15 @@ def dataloader(dataset: str) -> Dict[str, pd.DataFrame]:
     """
     dfs = dict()
     for sub_dataset in [section for section in CONFIGS.sections() if section.startswith(dataset)]:
-        params = dict(delimiter=CONFIGS.get(sub_dataset, "delimeter", ","), names=CONFIGS.get(sub_dataset, "headers").split(","))
-        if CONFIGS.get(sub_dataset, "timestamp", None):
+        params = dict(delimiter=CONFIGS.get(sub_dataset, "delimeter", fallback=","), names=CONFIGS.get(sub_dataset, "headers").split(","))
+        if CONFIGS.get(sub_dataset, "timestamp", fallback=None):
             params.update(dict(date_parser=CONFIGS.get(sub_dataset, "timestamp").split(",")))
         df = pd.read_csv(CONFIGS.get(sub_dataset, "file_path"), **params)
-        df.set_index(CONFIGS.get(sub_dataset, "index_key", None), inplace=True)
+        df.set_index(CONFIGS.get(sub_dataset, "index_key", fallback=None), inplace=True)
         dfs.update(dict(sub_dataset=df))
     
     # Return the dataframe
     return dfs
+
+# from dataloaders import dataloaders as dl
+# dl.dataloader('lfm_360k')
