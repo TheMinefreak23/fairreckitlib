c034698085e73917840d279300470e44cb8760b8
SanazNajiyan <45420543+SanazNajiyan@users.noreply.github.com>
2022-04-11 09:38:31 +0200

    updating dataloaders + create user-item matrix
    creating a general dataloaders for all datasets was not feasible, so I made an ABC=abstract class of dataloader to derive specific dataloader per dataset. Also I solved how this user-item matrix should be. Mapping the index of matrix to the id s is now feasible too

--- fair-rec-kit-lib/dataloaders/dataloaders.py
+++ fair-rec-kit-lib/dataloaders/dataloaders.py
@@ -5,35 +5,94 @@ Utrecht University within the Software Project course.
 © Copyright Utrecht University (Department of Information and Computing Sciences)
 
 """
-
+from abc import ABC
+from pyclbr import Class
 import pandas as pd
+import numpy as np
 from utility import get_configs
-from typing import Dict
+from typing import List
 import os
+from pandas.api.types import CategoricalDtype
+from scipy import sparse
+import zlib
 
-CONFIGS = get_configs(os.path.dirname(os.path.abspath(__file__)) + "\\config.ini")
-
-def dataloader(dataset: str) -> Dict[str, pd.DataFrame]:
+class dataloader_base(ABC):
+    """_summary_
     """
-    This function reads all the files of the dataset that its name is given,
-    and loads the content of the files in dataframes based on the read config file.
-    
-    Arguments:
-        dataset: The name of the dataset to be loaded
+    configs = None
+    _dataset_name = None
+    data_frames = {}
+    _user_cat = None
+    _item_cat = None
 
-    Returns:
-        dfs: a dictionary in which the name of the dataset's file is the key
-             and the content of the file is the value as a dataframe
+    def __init__(self) -> None:
+        self.configs = get_configs(os.path.dirname(os.path.abspath(__file__)) + "\\config.ini")
+
+class dataloader_360k(dataloader_base):
+    """_summary_
     """
-
-    dfs = dict()
-    for sub_dataset in [section for section in CONFIGS.sections() if section.startswith(dataset)]:
-        params = dict(delimiter=CONFIGS.get(sub_dataset, "delimiter", fallback=","), names=CONFIGS.get(sub_dataset, "headers").split(","), engine='python')
-        if CONFIGS.get(sub_dataset, "timestamp", fallback=None):
-            params.update(dict(parse_dates=CONFIGS.get(sub_dataset, "timestamp").split(",")))
-        df = pd.read_csv(CONFIGS.get(sub_dataset, "file_path"), **params)
-        # df.set_index(CONFIGS.get(sub_dataset, "index_key", fallback=None), inplace=True)
-        dfs.update({sub_dataset: df})
     
-    # Return the dataframe(s) in a dictionary
-    return dfs
+    def __init__(self):
+        super(dataloader_360k, self).__init__()
+        self._dataset_name = "lfm_360k"
+
+    def load_data(self) -> None:
+        """
+        This function reads all the files of the dataset that its name is given,
+        and loads the content of the files in dataframes based on the read config file.
+        """
+
+        for sub_dataset in [section for section in self.configs.sections() if section.startswith(self._dataset_name)]:
+            params = dict(delimiter=self.configs.get(sub_dataset, "delimiter", fallback=","),
+                          names=self.configs.get(sub_dataset, "headers").split(","),
+                          engine='python')
+            if self.configs.get(sub_dataset, "timestamp", fallback=None):
+                params.update(dict(parse_dates=self.configs.get(sub_dataset, "timestamp").split(",")))
+            df = pd.read_csv(self.configs.get(sub_dataset, "file_path"), **params)
+            self.data_frames.update({sub_dataset: df})
+    
+    def get_user_item_matrix(self) -> sparse.csr_matrix:
+        """
+        """
+        df = self.data_frames["lfm_360k_usersha1-artmbid-artname-plays"].copy()
+
+        df['user_id'] = df['user-mboxsha1'].map(lambda x: zlib.adler32(str(x).encode('utf-8', errors='ignore')))
+        df['artist_id'] = df['artist-name'].map(lambda x: zlib.adler32(str(x).encode('utf-8', errors='ignore')))
+        
+        self.data_frames["lfm_360k_usersha1-artmbid-artname-plays"] = df
+
+        users = df["user_id"].unique()
+        items = df["artist_id"].unique()
+        shape = (len(users), len(items))
+
+        # Create indices for users and items
+        self._user_cat = CategoricalDtype(categories=sorted(users), ordered=True)
+        self._item_cat = CategoricalDtype(categories=sorted(items), ordered=True)
+        user_index = df["user_id"].astype(self._user_cat).cat.codes
+        item_index = df["artist_id"].astype(self._item_cat).cat.codes
+
+        # Conversion via COO matrix
+        coo = sparse.coo_matrix((df["plays"].astype(np.float32), (user_index, item_index)), shape=shape)
+        return coo.tocsr()
+
+
+    def get_user_id(self, user_index: int) -> int:
+        """
+        """
+        df = pd.array([user_index])
+        user_ids = list(df.astype(self._user_cat).cat.categories)
+        return user_ids[0]
+
+    def get_item_id(self, item_index) -> List[int]:
+        """
+        """
+        df = pd.array([item_index])
+        item_ids = list(df.astype(self._item_cat).cat.categories)
+        return item_ids[0]
+
+def get_dataloader(dataset_name: str) -> dataloader_base:
+# def dataloader(dataset_name: str) -> dataloader_base:
+    """
+    """
+    if dataset_name == "lfm_360k":
+        return dataloader_360k()
