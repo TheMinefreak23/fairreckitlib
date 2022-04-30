5eccd50994f9be2a272686dd845693202c85565e
Roderick8488 <r.spaans88@gmail.com>
2022-04-30 11:06:19 +0200

    Modified ConfigParameters test
    - reusable add param function with assertions
    - using pytest raises functionality
    - fixed some linter issues

--- tests/test_config_params.py
+++ tests/test_config_params.py
@@ -86,7 +86,7 @@ def test_config_value_param(value_type, default_value, min_max_value, valid, inv
         print('ConfigValueParam invalid:', msg)
 
         # bool needs to be handled separately, because python considers it to be an int
-        if (isinstance(opt, float) or isinstance(opt, int)) and not isinstance(opt, bool):
+        if (isinstance(opt, (float, int))) and not isinstance(opt, bool):
             if opt < param.min_value:
                 assert value == param.min_value, 'Validated value should be clamped to minimum.'
             elif opt > param.max_value:
@@ -114,7 +114,7 @@ def test_config_parameters():
     config_parameters = get_empty_parameters()
     assert config_parameters.get_num_params() == 0, 'Empty parameters should have no entries.'
 
-    params = [
+    param_list = [
         create_bool_param('bool', True),
         ConfigOptionParam('option', str, 'a', ['a', 'b', 'c']),
         ConfigRandomParam('random'),
@@ -122,101 +122,80 @@ def test_config_parameters():
         ConfigValueParam('float', float, 0.5, (0.0, 1.0))
     ]
 
-    num_options = 0
-    num_values = 0
+    def add_param(param, param_exists):
+        if param_exists:
+            assert config_parameters.get_param(param.name) is not None, 'Param should exist.'
+        else:
+            assert config_parameters.get_param(param.name) is None, 'Param should not exist.'
 
-    # first pass: add everything to parameters successfully
-    for _, p in enumerate(params):
-        assert config_parameters.get_param(p.name) is None, 'Param should not exist.'
+        num_options = len(config_parameters.options)
+        num_values = len(config_parameters.values)
 
-        if isinstance(p, ConfigOptionParam):
-            if isinstance(p.value_type, bool):
+        if isinstance(param, ConfigOptionParam):
+            if isinstance(param.value_type, bool):
                 config_parameters.add_bool(
-                    p.name,
-                    p.default_value
+                    param.name,
+                    param.default_value
                 )
             else:
                 config_parameters.add_option(
-                    p.name,
-                    p.value_type,
-                    p.default_value,
-                    p.options
+                    param.name,
+                    param.value_type,
+                    param.default_value,
+                    param.options
                 )
 
             num_options += 1
 
-        elif isinstance(p, ConfigValueParam):
-            if isinstance(p, ConfigRandomParam):
-                config_parameters.add_random_seed(
-                    p.name
-                )
+        elif isinstance(param, ConfigValueParam):
+            if isinstance(param, ConfigRandomParam):
+                config_parameters.add_random_seed(param.name)
             else:
                 config_parameters.add_value(
-                    p.name,
-                    p.value_type,
-                    p.default_value,
-                    (p.min_value, p.max_value)
+                    param.name,
+                    param.value_type,
+                    param.default_value,
+                    (param.min_value, param.max_value)
                 )
 
             num_values += 1
 
         else:
-            raise NotImplementedError()
+            raise NotImplementedError('Unknown derived config param class.')
 
-        assert p.name in config_parameters.get_param_names(), \
+        assert param.name in config_parameters.get_param_names(), \
             'Param name should be available after adding.'
         assert config_parameters.get_num_params() == num_options + num_values, \
             'Number of params should the same as the amount of options and values together.'
         assert len(config_parameters.options) == num_options, \
-            'Number of options params should be the same as the amount of added options.'
+            'Number of option params should be the same as the amount of added options.'
         assert len(config_parameters.values) == num_values, \
             'Number of value params should be the same as the amount of added values.'
 
-    # second pass: add everything to parameters again, unsuccessfully
-    for _, p in enumerate(params):
-        assert config_parameters.get_param(p.name) is not None, 'Param should exist.'
 
-        error_raised = False
+    # first pass: add everything to parameters successfully
+    for _, config_param in enumerate(param_list):
+        add_param(config_param, False)
 
-        try:
-            if isinstance(p, ConfigOptionParam):
-                if isinstance(p.value_type, bool):
-                    config_parameters.add_bool(
-                        p.name,
-                        p.default_value
-                    )
-                else:
-                    config_parameters.add_option(
-                        p.name,
-                        p.value_type,
-                        p.default_value,
-                        p.options
-                    )
-            elif isinstance(p, ConfigValueParam):
-                num_values += 1
+    # second pass: add everything to parameters again, but unsuccessfully
+    for _, config_param in enumerate(param_list):
+        num_params = config_parameters.get_num_params()
+        num_options = len(config_parameters.options)
+        num_values = len(config_parameters.values)
 
-                if isinstance(p, ConfigRandomParam):
-                    config_parameters.add_random_seed(
-                        p.name
-                    )
-                else:
-                    config_parameters.add_value(
-                        p.name,
-                        p.value_type,
-                        p.default_value,
-                        (p.min_value, p.max_value)
-                    )
-            else:
-                raise NotImplementedError()
-        except KeyError:
-            error_raised = True
-        finally:
-            assert error_raised, 'Error should be raised on adding duplicate param name.'
+        pytest.raises(KeyError, add_param, config_param, True)
+
+        assert config_parameters.get_num_params() == num_params, \
+            'Number of params should be unchanged.'
+        assert len(config_parameters.options) == num_options, \
+            'Number of option params should be unchanged.'
+        assert len(config_parameters.values) == num_values, \
+            'Number of value params should be unchanged.'
 
     # check parameter defaults
     param_defaults = config_parameters.get_defaults()
-    for _, p in enumerate(params):
-        assert p.name in param_defaults, \
+    for _, config_param in enumerate(param_list):
+        assert config_param.name in param_defaults, \
             'Original param name should be present in the defaults.'
-        assert p.default_value == param_defaults[p.name], \
+        assert config_param.default_value == param_defaults[config_param.name], \
             'Param default should be the same as the original default value.'
