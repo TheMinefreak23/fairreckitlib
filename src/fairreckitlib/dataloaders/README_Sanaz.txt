1d3befc5c6a63bcd453a67960b2adb04af08eefd
SanazNajiyan <45420543+SanazNajiyan@users.noreply.github.com>
2022-05-01 17:07:00 +0200

    doc strings and comments
    dataloaders.py
    utility.py

--- src/fairreckitlib/dataloaders/README_Sanaz.txt
+++ src/fairreckitlib/dataloaders/README_Sanaz.txt
@@ -11,4 +11,10 @@ README_Sanaz
 #for example if you wanted to check whether remove works correctly, first check what are the attributes then do the drop
 >>> loader.ui_data_frame.info()
 >>> loader.ui_data_frame.drop(columns=['item'], inplace=True)
-#also it is easier to work with your sample dataset. you could make it as follows:
+
+#also it is easier to work with your sample dataset. I already made the function in utility. note that 
+the function takes the path of the text file and the number of lines to be copied to the sample file.
+you could make it as follows:
+>>> import fairreckitlib.dataloaders.utility
+>>> create_sample("file path", 500)
+example of a file path: ../Datasets/LFM_360K/usersha1-artmbid-artname-plays.tsv
--- src/fairreckitlib/dataloaders/dataloaders.py
+++ src/fairreckitlib/dataloaders/dataloaders.py
@@ -18,7 +18,8 @@ from scipy import sparse
 from .utility import get_configs
 
 class DataloaderBase(ABC):
-    """_summary_
+    """
+    Abstract class
     """
 
     def __init__(self, config_path: str = "") -> None:
@@ -34,27 +35,27 @@ class DataloaderBase(ABC):
     @abstractmethod
     def load_data(self) -> None:
         """
-        _summary_
+        the abstract method obliges the developer to overwrite the method
         """
         raise NotImplementedError()
 
     @abstractmethod
     def filter_df(self, filters: Dict[str, Any]) -> None:
         """
-        _summary_
+        abstract method of the class
         """
         raise NotImplementedError()
 
     @abstractmethod
     def df_add_column(self, add_columns: List[str]) -> None:
         """
-        __summary__
+        abstract method of the class to add columns
         """
         raise NotImplementedError()
 
     def df_remove_column(self, column_list: List[str]) -> None:
         """
-        __summary__
+        general method of the class to remove columns
         """
         headers = self.ui_data_frame.columns
         common_elements = np.intersect1d(column_list, headers)
@@ -64,7 +65,7 @@ class DataloaderBase(ABC):
 
     def df_formatter(self, columns_add: List[str] = None, columns_remove: List[str] = None) -> None:
         """
-        __summary__
+        general method of the class to add/remove columns and returns the updated data frame
         """
         if columns_add:
             self.df_add_column(columns_add)
@@ -104,7 +105,8 @@ class DataloaderBase(ABC):
     @classmethod
     def get_file_path(cls, file_path: str, file_name: str) -> str:
         """
-        _summary_
+        this function takes the class and file path and file name to make a string of file path
+        where dataset exists
         """
         if not os.path.isdir(file_path):
             raise IOError('Failed to initialize DataRegistry: '
@@ -131,7 +133,7 @@ class DataloaderBase(ABC):
 
     def save_to_tsv(self, file_path: str) -> None:
         """
-        _summary_
+        this function takes the file path and saves tsv version of the data frame
         """
         self.ui_data_frame.to_csv(file_path, header=False, sep='\t', index=False)
 
@@ -199,12 +201,13 @@ class Dataloader360K(DataloaderBase):
 
     def df_add_column(self, add_columns: List[str]) -> None:
         """
-        __summary__
+        this function gets the list of columns to be added and add them to the dataframe
         """
         headers = ["user", "gender", "age", "country"]
         common_elements = np.intersect1d(add_columns, headers)
         if not common_elements:
             return
+        # in profile.tsv there is more user-related info that is why we use this file 
         file_name = "usersha1-profile.tsv"
         fields_map = {"gender": 1, "age": 2, "country": 3}
         use_cols = [0]
@@ -290,15 +293,18 @@ class Dataloader1B(DataloaderBase):
     def __init__(self, config_path: str = "") -> None:
         super().__init__(config_path)
         self._dataset_name = "LFM-1B"
+        # we need to union the sets in order to get rid of duplicates 
         self._filter_options = ft.reduce(lambda x, y: set(x) | set(y), self.options.values())
 
     def load_data(self) -> None:
         """
-        This function reads all the files of the 1b dataset that its name is given,
-        and loads the content of the files in dataframes based on the read config file.
+        This function loads the mat_file
+        in order to make user_artist_rating dataframe
         """
         mat_file = self.get_file_path(self.configs.get(self._dataset_name, 'file_path'),
                                       self.configs.get(self._dataset_name, 'file_name'))
+        # Read the user-artist-matrix and corresponding artist and user indices from Matlab file
+        # source: http://www.cp.jku.at/datasets/LFM-1b/LFM-1b_stats.py
         mat_data = h5py.File(mat_file, 'r')
         user_ids = [item[0] for item in np.array(mat_data.get('idx_users')).astype(np.int64)]
         artist_ids = np.array(mat_data.get('idx_artists')).astype(np.int64)
@@ -306,12 +312,14 @@ class Dataloader1B(DataloaderBase):
                                        mat_data['/LEs/']["ir"],
                                        mat_data['/LEs/']["jc"])).transpose()
         for i, user_i in enumerate(user_ids):
+            # pc_i represent the rows(users), idx_nz is the non-zero value (ratings)
             pc_i = ua_matrix.getrow(i).toarray()[0]
             idx_nz = np.nonzero(pc_i)
             ratings_i = pc_i[idx_nz]     # get playcount vector for user i
             artist_ids_i = [item[0] for item in artist_ids[idx_nz]]
             data_frame = list(zip([user_i] * len(artist_ids_i), artist_ids_i, ratings_i))
             data_frame = pd.DataFrame(data_frame, columns=['user', 'item', 'rating'])
+            # if this is the first time 
             if self.ui_data_frame.empty:
                 self.ui_data_frame = data_frame.copy()
             else:
@@ -319,10 +327,15 @@ class Dataloader1B(DataloaderBase):
 
     def df_add_column(self, add_columns: List[str]) -> None:
         """
-        __summary__
+        this function adds list of columns to our data frame
         """
+        # our dataframe's headers are user-item-rating
+        # renaming columns to be able to join the data frames 
         self.ui_data_frame.rename(columns = {'user': 'user-id', 'item': 'artist-id'},
                                   inplace = True)
+        # if album-name should be added we need LES and albums tables
+        # to merge on user-id and artist-id (in general item related columns) 
+        # and later be added to the dataframe 
         if "album-name" in add_columns:
             df_base = self.read_file("LEs", ["user-id", "artist-id", "album-id"])
             df_album = self.read_file("albums", self.options["albums"])
@@ -352,6 +365,7 @@ class Dataloader1B(DataloaderBase):
             self.ui_data_frame.fillna(value={"artist-name": ""}, inplace=True)
         common_elements = list(np.intersect1d(add_columns, self.options['LEs']))
         if common_elements:
+            # we need user-id for join operation
             if not "user-id" in common_elements:
                 common_elements.insert(0, "user-id")
             data_frame = self.read_file("LEs", common_elements)
@@ -371,11 +385,14 @@ class Dataloader1B(DataloaderBase):
         common_elements = list(np.intersect1d(add_columns, self.options['users_additional']))
         if common_elements:
             if "user-id" not in common_elements:
+                # user-id needs to be there to perform the join. if it is not among add_columns, I need to 
+                # add it artificially and then omit the column because in the end I don't need user-id column
                 common_elements.insert(0, "user-id")
             data_frame = self.read_file("users_additional", common_elements)
             self.ui_data_frame = pd.merge(self.ui_data_frame, data_frame, on=["user-id"],
                                           how="left")
-            _ = common_elements.pop(0)
+            _ = common_elements.pop(0) #_wild card: we don't need the user-id anymore
+            # we make a dictionary to mark the na_values as -1 to feed it to our data frame
             na_values = dict(zip(common_elements, [-1.0] * len(common_elements)))
             self.ui_data_frame.fillna(value=na_values, inplace=True)
 
@@ -384,7 +401,8 @@ class Dataloader1B(DataloaderBase):
 
     def read_file(self, f_name: str, r_columns: List[str]) -> pd.DataFrame:
         """
-        _summary_
+        this function takes the file name and list of columns to be read and returns the data frame
+        example: LFM-1b_users.txt
         """
         file_name = f"LFM-1b_{f_name}.txt"
         params = dict(delimiter=self.configs.get("common", "DELIMITER", fallback=","),
@@ -396,7 +414,8 @@ class Dataloader1B(DataloaderBase):
 
     def filter_df(self, filters: Dict[str, Any]) -> None:
         """
-        _summary_
+        this function takes a dictionary of string of any type(could be int, tuple,..) 
+        and returns the updated data frame
         """
         headers = self.ui_data_frame.columns
         common_elements = list(np.intersect1d(list(filters.keys()), headers))
@@ -428,7 +447,8 @@ def get_dataloader(dataset_name: str, config_path: str = "") -> DataloaderBase:
 
 def explore_datasets(config_file_path: str, data_dir: str) -> None:
     """
-    _summary_
+    this function takes the path of the config file and the directory contains the datasets
+    and writes the configuration file
     """
     if not os.path.isdir(data_dir):
         raise IOError('Failed to initialize DataRegistry: '
@@ -443,9 +463,10 @@ def explore_datasets(config_file_path: str, data_dir: str) -> None:
 
         dataset_path = os.path.join(dataset_dir, file_name + '.tsv')
         if not os.path.isfile(dataset_path):
+            # making the file
             with open(dataset_path, 'a', encoding="utf-8") as dfile:
                 dfile.write('')
-
+        # now for each section make the default value
         import configparser
         config = configparser.RawConfigParser()
         if not config.has_section(file_name):
