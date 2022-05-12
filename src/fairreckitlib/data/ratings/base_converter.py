ce717610f215a6b44389e08b9b0690b1dcf03b3b
Isabelle van Driessel <si.vd@outlook.com>
2022-05-12 19:23:37 +0200

    added type annotations and updated docstrings to fit pep257 conventions, whole ratings folder is done

--- src/fairreckitlib/data/ratings/base_converter.py
+++ src/fairreckitlib/data/ratings/base_converter.py
@@ -5,26 +5,35 @@ Utrecht University within the Software Project course.
 """
 
 from abc import ABCMeta, abstractmethod
+from typing import Dict, Any
 
+import pandas as pd
 
 class RatingConverter(metaclass=ABCMeta):
     """Base class for FairRecKit rating converters.
 
     A converter is used to convert ratings of a dataframe.
     """
-    def __init__(self, name, params):
+
+    def __init__(self, name: str, params: Dict[str, Any]):
+        """Construct the Rating Converter.
+
+        Args:
+            name: the name of the converter.
+            params: the converter parameters.
+        """
         self.__name = name
         self.params = params
 
-    def get_name(self):
-        """Gets the name of the converter.
+    def get_name(self) -> str:
+        """Get the name of the converter.
 
         Returns:
             (str) the converter name.
         """
         return self.__name
 
-    def get_params(self):
+    def get_params(self) -> Dict[str, Any]:
         """Get the parameters of the converter.
 
         Returns:
@@ -33,14 +42,14 @@ class RatingConverter(metaclass=ABCMeta):
         return dict(self.params)
 
     @abstractmethod
-    def run(self, dataframe):
-        """Runs the converter on the specified dataframe.
+    def run(self, dataframe: pd.DataFrame) -> tuple[pd.DataFrame, str]:
+        """Run the converter on the specified dataframe.
 
         Args:
-            dataframe(pandas.DataFrame): with at least the 'rating' column.
+            dataframe: with at least the 'rating' column.
 
         Returns:
-            dataframe(pandas.DataFrame): the converter dataframe.
-            rating_type(str): the converted type of rating, either 'explicit' or 'implicit'.
+            dataframe: the converted dataframe.
+            rating_type: the converted type of rating, either 'explicit' or 'implicit'.
         """
         raise NotImplementedError()
--- src/fairreckitlib/data/ratings/rating_converter_factory.py
+++ src/fairreckitlib/data/ratings/rating_converter_factory.py
@@ -4,8 +4,9 @@ Utrecht University within the Software Project course.
 © Copyright Utrecht University (Department of Information and Computing Sciences)
 """
 
+from typing import Dict, Any
 from ...core.config_params import ConfigParameters
-from ...core.factories import create_factory_from_list
+from ...core.factories import Factory, create_factory_from_list
 from .range_converter import RangeConverter
 from .kl_converter import KLConverter
 
@@ -15,7 +16,8 @@ CONVERTER_RANGE = 'range'
 CONVERTER_KL = 'kl'
 
 
-def create_rating_converter_factory():
+def create_rating_converter_factory() -> Factory:
+    """Create the rating converter factory."""
     return create_factory_from_list(KEY_RATING_CONVERTER, [
         (CONVERTER_KL,
          _create_kl_converter,
@@ -28,21 +30,21 @@ def create_rating_converter_factory():
     ])
 
 
-def _create_range_converter(name, params):
+def _create_range_converter(name: str, params: Dict[str, Any]) -> RangeConverter:
     return RangeConverter(name, params)
 
 
-def _create_range_converter_params():
+def _create_range_converter_params() -> ConfigParameters:
     params = ConfigParameters()
     params.add_value('upper_bound', float, 1.0, (1.0, 1000.0))
     return params
 
 
-def _create_kl_converter(name, params):
+def _create_kl_converter(name: str, params: Dict[str, Any]) -> KLConverter:
     return KLConverter(name, params)
 
 
-def _create_kl_converter_params():
+def _create_kl_converter_params() -> ConfigParameters:
     methods = [
         'APC',
         'ALC'
