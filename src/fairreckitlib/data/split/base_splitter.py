1d60364e5691c9c20445bf6a5b60a2893797d96d
Isabelle van Driessel <si.vd@outlook.com>
2022-05-12 15:47:48 +0200

    added type annotation and updated docstrings to fit pep257 conventions, whole split folder is done

--- src/fairreckitlib/data/split/base_splitter.py
+++ src/fairreckitlib/data/split/base_splitter.py
@@ -5,6 +5,8 @@ Utrecht University within the Software Project course.
 """
 
 from abc import ABCMeta, abstractmethod
+from typing import Dict, Any
+import pandas as pd
 
 
 class DataSplitter(metaclass=ABCMeta):
@@ -12,19 +14,26 @@ class DataSplitter(metaclass=ABCMeta):
 
     A splitter is used to split a dataframe into a train and test set.
     """
-    def __init__(self, name, params):
+
+    def __init__(self, name: str, params: Dict[str, Any]):
+        """Construct the base splitter.
+
+        Args:
+            name: the name of the splitter.
+            params: a dictionary containing the parameters for the splitter.
+        """
         self.__name = name
         self.__params = params
 
-    def get_name(self):
-        """Gets the name of the splitter.
+    def get_name(self) -> str:
+        """Get the name of the splitter.
 
         Returns:
             (str) the splitter name.
         """
         return self.__name
 
-    def get_params(self):
+    def get_params(self) -> Dict[str, Any]:
         """Get the parameters of the splitter.
 
         Returns:
@@ -33,15 +42,14 @@ class DataSplitter(metaclass=ABCMeta):
         return dict(self.__params)
 
     @abstractmethod
-    def run(self, dataframe, test_ratio):
-        """Runs the splitter on the specified dataframe.
+    def run(self, dataframe: pd.DataFrame, test_ratio: float) -> tuple[pd.DataFrame, pd.DataFrame]:
+        """Run the splitter on the specified dataframe.
 
         Args:
-            dataframe(pandas.DataFrame): with at least the 'user' column.
-            test_ratio(float): the fraction of users to use for testing.
+            dataframe: with at least the 'user' column.
+            test_ratio: the fraction of users to use for testing.
 
         Returns:
-            train_set(pandas.DataFrame): the train set of the split.
-            test_set(pandas.DataFrame): the test set of the split.
+            train_set, test_set: the train and test set of the split.
         """
         raise NotImplementedError()
--- src/fairreckitlib/data/split/random_splitter.py
+++ src/fairreckitlib/data/split/random_splitter.py
@@ -5,6 +5,7 @@ Utrecht University within the Software Project course.
 """
 
 import lenskit.crossfold as xf
+import pandas as pd
 
 from .base_splitter import DataSplitter
 
@@ -15,7 +16,16 @@ class RandomSplitter(DataSplitter):
     Splits the dataframe into a train and test set randomly.
     """
 
-    def run(self, dataframe, test_ratio):
+    def run(self, dataframe: pd.DataFrame, test_ratio: float) -> tuple[pd.DataFrame, pd.DataFrame]:
+        """Split the dataframe into a train and test set.
+
+        Args:
+            dataframe: with at least the 'user' column.
+            test_ratio: the fraction of users to use for testing.
+
+        Returns:
+            train_set, test_set: the train and test set.
+        """
         # TODO set random seed as specified in params
         frac = xf.SampleFrac(test_ratio)
         for train_set, test_set in xf.partition_users(dataframe, 1, frac):
